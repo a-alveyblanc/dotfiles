a1ef84b5de30753a7fcd894871d82bfb0fe1a25b
Addison Alvey-Blanco <aja13@illinois.edu>
2024-07-02 19:34:14 -0600

    add script for checking wireguard status

---
+++ scripts/restart-wireguard.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ "$1" = "" ]; then
+    echo "Provide an interface as a command line argument."
+    exit 1
+fi
+
+new_interface=$1
+old_interface=$(wg) | grep "interface" | sed -r "s/interface: //"
+
+echo $old_interface
+echo $new_interface
+
+# if [ "$old_interface" = "" ]; then
+#     wg-quick up $new_interface 
+# elif [ "$old_interface" != "$new_interface" ]; then
+#     wg-quick down $old_interface
+#     wg-quick up $new_interface
+# elif [ "$old_interface" = "$new_interface" ]; then
+#     echo "Interface already running. Running `wg` to show status."
+#     exit 0
+# fi
