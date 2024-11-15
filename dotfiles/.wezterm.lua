920218d187a91d315909b9e2ed60086ba643cf9a
Addison Alvey-Blanco <aja13@illinois.edu>
2024-11-14 20:44:27 -0600

    more basic wezterm configs

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -6,4 +6,7 @@ config.color_scheme = 'Catppuccin Macchiato'
 config.font = wezterm.font("Iosevka Term")
 config.font_size = 12.0
 
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+
 return config
